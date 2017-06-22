5f048f5ea1e4c4e544b60433aaf24239dab4ef73
Stefan Schuetz <stefan.schuetz@corona-systems.io>
2017-06-22 16:10:51 +0200

    Added simple p7zip_list() function

--- p7zip.c
+++ p7zip.c
@@ -28,6 +28,7 @@
 #include "php_p7zip.h"
 
 #include "lzma-sdk/C/7zCrc.h"
+#include "lzma-sdk/C/7zBuf.h"
 
 /* If you declare any globals in php_p7zip.h uncomment this:
 ZEND_DECLARE_MODULE_GLOBALS(p7zip)
@@ -60,14 +61,6 @@ static void php_p7zip_init_globals(zend_p7zip_globals *p7zip_globals)
 
 //static int le_p7zip_descriptor;
 
-static void* SzAlloc(void *p, size_t size){
-    return emalloc(size);
-}
-
-static void SzFree(void *p, void *address){
-  efree(address);
-}
-
 static void p7zip_free(zend_resource* rsrc){
     p7zip_file_t* file = (p7zip_file_t*) rsrc->ptr;
     
@@ -78,6 +71,304 @@ static void p7zip_free(zend_resource* rsrc){
     }
     
     rsrc->ptr = NULL;
+}
+
+static void* SzAlloc(void *p, size_t size){
+    return emalloc(size);
+}
+
+static void SzFree(void *p, void *address){
+    efree(address);
+}
+
+static ISzAlloc g_Alloc = { SzAlloc, SzFree };
+
+static int Buf_EnsureSize(CBuf *dest, size_t size){
+    if (dest->size >= size)
+        return 1;
+    Buf_Free(dest, &g_Alloc);
+    return Buf_Create(dest, size, &g_Alloc);
+}
+
+#ifndef _WIN32
+#define _USE_UTF8
+#endif
+
+/* #define _USE_UTF8 */
+
+#ifdef _USE_UTF8
+
+#define _UTF8_START(n) (0x100 - (1 << (7 - (n))))
+
+#define _UTF8_RANGE(n) (((UInt32)1) << ((n) * 5 + 6))
+
+#define _UTF8_HEAD(n, val) ((Byte)(_UTF8_START(n) + (val >> (6 * (n)))))
+#define _UTF8_CHAR(n, val) ((Byte)(0x80 + (((val) >> (6 * (n))) & 0x3F)))
+
+static size_t Utf16_To_Utf8_Calc(const UInt16 *src, const UInt16 *srcLim){
+    size_t size = 0;
+    for (;;){
+        UInt32 val;
+        if (src == srcLim)
+            return size;
+        
+        size++;
+        val = *src++;
+        
+        if (val < 0x80)
+            continue;
+        
+        if (val < _UTF8_RANGE(1)){
+            size++;
+            continue;
+        }
+        
+        if (val >= 0xD800 && val < 0xDC00 && src != srcLim){
+            UInt32 c2 = *src;
+            if (c2 >= 0xDC00 && c2 < 0xE000){
+                src++;
+                size += 3;
+                continue;
+            }
+        }
+        
+        size += 2;
+    }
+}
+
+static Byte *Utf16_To_Utf8(Byte *dest, const UInt16 *src, const UInt16 *srcLim){
+    for (;;){
+        UInt32 val;
+        if (src == srcLim)
+            return dest;
+        
+        val = *src++;
+        
+        if (val < 0x80){
+            *dest++ = (char)val;
+            continue;
+        }
+        
+        if (val < _UTF8_RANGE(1)){
+            dest[0] = _UTF8_HEAD(1, val);
+            dest[1] = _UTF8_CHAR(0, val);
+            dest += 2;
+            continue;
+        }
+        
+        if (val >= 0xD800 && val < 0xDC00 && src != srcLim){
+            UInt32 c2 = *src;
+            if (c2 >= 0xDC00 && c2 < 0xE000){
+                src++;
+                val = (((val - 0xD800) << 10) | (c2 - 0xDC00)) + 0x10000;
+                dest[0] = _UTF8_HEAD(3, val);
+                dest[1] = _UTF8_CHAR(2, val);
+                dest[2] = _UTF8_CHAR(1, val);
+                dest[3] = _UTF8_CHAR(0, val);
+                dest += 4;
+                continue;
+            }
+        }
+        
+        dest[0] = _UTF8_HEAD(2, val);
+        dest[1] = _UTF8_CHAR(1, val);
+        dest[2] = _UTF8_CHAR(0, val);
+        dest += 3;
+    }
+}
+
+static SRes Utf16_To_Utf8Buf(CBuf *dest, const UInt16 *src, size_t srcLen){
+    size_t destLen = Utf16_To_Utf8_Calc(src, src + srcLen);
+    destLen += 1;
+    if (!Buf_EnsureSize(dest, destLen))
+        return SZ_ERROR_MEM;
+    *Utf16_To_Utf8(dest->data, src, src + srcLen) = 0;
+    return SZ_OK;
+}
+
+#endif
+
+static SRes Utf16_To_Char(CBuf *buf, const UInt16 *s
+    #ifndef _USE_UTF8
+    , UINT codePage
+    #endif
+    ){
+    unsigned len = 0;
+    for (len = 0; s[len] != 0; len++);
+    
+    #ifndef _USE_UTF8
+    {
+        unsigned size = len * 3 + 100;
+        if (!Buf_EnsureSize(buf, size))
+            return SZ_ERROR_MEM;
+        {
+            buf->data[0] = 0;
+            if (len != 0){
+                char defaultChar = '_';
+                BOOL defUsed;
+                unsigned numChars = 0;
+                numChars = WideCharToMultiByte(codePage, 0, s, len, (char *)buf->data, size, &defaultChar, &defUsed);
+                if (numChars == 0 || numChars >= size)
+                    return SZ_ERROR_FAIL;
+                buf->data[numChars] = 0;
+            }
+            return SZ_OK;
+        }
+    }
+    #else
+    return Utf16_To_Utf8Buf(buf, s, len);
+    #endif
+}
+
+#ifdef _WIN32
+  #ifndef USE_WINDOWS_FILE
+    static UINT g_FileCodePage = CP_ACP;
+  #endif
+  #define MY_FILE_CODE_PAGE_PARAM ,g_FileCodePage
+#else
+  #define MY_FILE_CODE_PAGE_PARAM
+#endif
+
+static WRes MyCreateDir(const UInt16 *name){
+    #ifdef USE_WINDOWS_FILE
+    
+    return CreateDirectoryW(name, NULL) ? 0 : GetLastError();
+    
+    #else
+    
+    CBuf buf;
+    WRes res;
+    Buf_Init(&buf);
+    RINOK(Utf16_To_Char(&buf, name MY_FILE_CODE_PAGE_PARAM));
+    
+    res =
+    #ifdef _WIN32
+    _mkdir((const char *)buf.data)
+    #else
+    mkdir((const char *)buf.data, 0777)
+    #endif
+    == 0 ? 0 : errno;
+    Buf_Free(&buf, &g_Alloc);
+    return res;
+    
+    #endif
+}
+
+static WRes OutFile_OpenUtf16(CSzFile *p, const UInt16 *name){
+    #ifdef USE_WINDOWS_FILE
+    return OutFile_OpenW(p, name);
+    #else
+    CBuf buf;
+    WRes res;
+    Buf_Init(&buf);
+    RINOK(Utf16_To_Char(&buf, name MY_FILE_CODE_PAGE_PARAM));
+    res = OutFile_Open(p, (const char *)buf.data);
+    Buf_Free(&buf, &g_Alloc);
+    return res;
+    #endif
+}
+
+static SRes PrintString(const UInt16 *s){
+    CBuf buf;
+    SRes res;
+    Buf_Init(&buf);
+    res = Utf16_To_Char(&buf, s
+    #ifndef _USE_UTF8
+    , CP_OEMCP
+    #endif
+    );
+    if (res == SZ_OK)
+        fputs((const char *)buf.data, stdout);
+    Buf_Free(&buf, &g_Alloc);
+    return res;
+}
+
+static void UInt64ToStr(UInt64 value, char *s){
+    char temp[32];
+    int pos = 0;
+    do{
+        temp[pos++] = (char)('0' + (unsigned)(value % 10));
+        value /= 10;
+    }
+    while (value != 0);
+    do
+        *s++ = temp[--pos];
+    while (pos);
+    *s = '\0';
+}
+
+static char *UIntToStr(char *s, unsigned value, int numDigits){
+    char temp[16];
+    int pos = 0;
+    do
+        temp[pos++] = (char)('0' + (value % 10));
+    while (value /= 10);
+    for (numDigits -= pos; numDigits > 0; numDigits--)
+        *s++ = '0';
+    do
+        *s++ = temp[--pos];
+    while (pos);
+    *s = '\0';
+    return s;
+}
+
+static void UIntToStr_2(char *s, unsigned value){
+    s[0] = (char)('0' + (value / 10));
+    s[1] = (char)('0' + (value % 10));
+}
+
+#define PERIOD_4 (4 * 365 + 1)
+#define PERIOD_100 (PERIOD_4 * 25 - 1)
+#define PERIOD_400 (PERIOD_100 * 4 + 1)
+
+static void ConvertFileTimeToString(const CNtfsFileTime *nt, char *s){
+    unsigned year, mon, hour, min, sec;
+    Byte ms[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    unsigned t;
+    UInt32 v;
+    UInt64 v64 = nt->Low | ((UInt64)nt->High << 32);
+    v64 /= 10000000;
+    sec = (unsigned)(v64 % 60); v64 /= 60;
+    min = (unsigned)(v64 % 60); v64 /= 60;
+    hour = (unsigned)(v64 % 24); v64 /= 24;
+    
+    v = (UInt32)v64;
+    
+    year = (unsigned)(1601 + v / PERIOD_400 * 400);
+    v %= PERIOD_400;
+    
+    t = v / PERIOD_100; if (t ==  4) t =  3; year += t * 100; v -= t * PERIOD_100;
+    t = v / PERIOD_4;   if (t == 25) t = 24; year += t * 4;   v -= t * PERIOD_4;
+    t = v / 365;        if (t ==  4) t =  3; year += t;       v -= t * 365;
+    
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+        ms[1] = 29;
+    for (mon = 0;; mon++){
+        unsigned d = ms[mon];
+        if (v < d)
+            break;
+        v -= d;
+    }
+    s = UIntToStr(s, year, 4); *s++ = '-';
+    UIntToStr_2(s, mon + 1); s[2] = '-'; s += 3;
+    UIntToStr_2(s, (unsigned)v + 1); s[2] = ' '; s += 3;
+    UIntToStr_2(s, hour); s[2] = ':'; s += 3;
+    UIntToStr_2(s, min); s[2] = ':'; s += 3;
+    UIntToStr_2(s, sec); s[2] = 0;
+}
+
+static void GetAttribString(UInt32 wa, Bool isDir, char *s){
+    #ifdef USE_WINDOWS_FILE
+    s[0] = (char)(((wa & FILE_ATTRIBUTE_DIRECTORY) != 0 || isDir) ? 'D' : '.');
+    s[1] = (char)(((wa & FILE_ATTRIBUTE_READONLY ) != 0) ? 'R': '.');
+    s[2] = (char)(((wa & FILE_ATTRIBUTE_HIDDEN   ) != 0) ? 'H': '.');
+    s[3] = (char)(((wa & FILE_ATTRIBUTE_SYSTEM   ) != 0) ? 'S': '.');
+    s[4] = (char)(((wa & FILE_ATTRIBUTE_ARCHIVE  ) != 0) ? 'A': '.');
+    s[5] = 0;
+    #else
+    s[0] = (char)(((wa & (1 << 4)) != 0 || isDir) ? 'D' : '.');
+    s[1] = 0;
+    #endif
 }
 
 /* {{{ proto Resource p7zip_open(string filename)
@@ -176,18 +467,21 @@ PHP_FUNCTION(p7zip_test){
     size_t outBufferSize = 0;
     UInt32 i;
     SRes res;
-    size_t offset = 0;
-    size_t outSizeProcessed = 0;
-    unsigned isDir;
-    
+  
     for (i = 0; i < file->db.NumFiles; i++){
-        isDir = SzArEx_IsDir(&file->db, i);
+        size_t offset = 0;
+        size_t outSizeProcessed = 0;
+        
+        unsigned isDir = SzArEx_IsDir(&file->db, i);
+        
         if(isDir)
             continue;
+        
         res = SzArEx_Extract(&file->db, &file->lookStream.s, i,
               &blockIndex, &outBuffer, &outBufferSize,
               &offset, &outSizeProcessed,
               &file->allocImp, &file->allocTempImp);
+        
         if (res != SZ_OK)
             break;
     }
@@ -198,6 +492,68 @@ PHP_FUNCTION(p7zip_test){
         RETURN_LONG(res);
     
     RETURN_TRUE;
+    
+}
+
+PHP_FUNCTION(p7zip_list){
+    zval* val;
+    p7zip_file_t* file;
+    zend_bool full_info = 0;
+    
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "r|b", &val, &zend_bool) == FAILURE) {
+        return;
+    }
+
+    if ((file = (p7zip_file_t*) zend_fetch_resource(Z_RES_P(val), le_p7zip_name, le_p7zip)) == NULL) {
+        RETURN_FALSE;
+    }
+    
+    UInt32 i;
+    SRes res;
+    UInt16 *temp = NULL;
+    size_t tempSize = 0;
+    
+    HashTable* ht;
+    if(full_info)
+        zend_init_hashtable(ht, file->db.NumFiles, NULL, NULL, 0);
+    else
+        array_init(return_value, file->db.NumFiles);
+    
+    for (i = 0; i < file->db.NumFiles; i++){
+        smart_string filename = {0};
+        size_t len;
+        unsigned isDir = SzArEx_IsDir(&file->db, i);
+        len = SzArEx_GetFileNameUtf16(&file->db, i, NULL);
+        
+        if (len > tempSize){
+          SzFree(NULL, temp);
+          tempSize = len;
+          temp = (UInt16 *)SzAlloc(NULL, tempSize * sizeof(temp[0]));
+          if (!temp){
+            res = SZ_ERROR_MEM;
+            break;
+          }
+        }
+        
+        if (res != SZ_OK)
+            break;  
+        
+        SzArEx_GetFileNameUtf16(&file->db, i, temp);
+        
+        smart_str_appendl(&filename, temp, len);
+        
+        if(isDir)
+            smart_str_appendl(&filename, "/", sizeof("/") - 1);
+        
+        if(add_next_index_string(return_value, filename.s, 1) == FAILURE){
+            RETURN_FALSE;
+        }
+    }
+    
+    IAlloc_Free(&file->allocImp, outBuffer);
+    
+    if(res != SZ_OK)
+        RETURN_LONG(res);
     
 }
 
@@ -279,6 +635,11 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_p7zip_test, 0, 0, 1)
 	ZEND_ARG_INFO(0, handle)
 ZEND_END_ARG_INFO()
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_p7zip_list, 0, 0, 1)
+	ZEND_ARG_INFO(0, handle)
+        ZEND_ARG_INFO(0, full_info)
+ZEND_END_ARG_INFO()
+
 /* {{{ p7zip_functions[]
  *
  * Every user visible function must have an entry in p7zip_functions[].
@@ -287,6 +648,7 @@ const zend_function_entry p7zip_functions[] = {
         PHP_FE(p7zip_open, arginfo_p7zip_open)
         PHP_FE(p7zip_close, arginfo_p7zip_close)
         PHP_FE(p7zip_test, arginfo_p7zip_test)
+        PHP_FE(p7zip_list, arginfo_p7zip_list)
 	PHP_FE_END	/* Must be the last line in p7zip_functions[] */
 };
 /* }}} */
---
+++ tests/p7zip_list.phpt
@@ -0,0 +1,15 @@
+--TEST--
+p7zip_test() function
+--SKIPIF--
+<?php if (!extension_loaded("p7zip")) print "skip"; ?>
+--FILE--
+<?php
+$handle = p7zip_open(dirname(__FILE__)."/test.7z");
+var_dump(p7zip_test($handle));
+p7zip_close($handle);
+?>
+--EXPECT--
+array(1) {
+  [0]=>
+  string(9) "test.text"
+}
