6fdf424247f7aedc1f31e705d687c018cf2ba07e
Stefan Schuetz <stefan.schuetz@corona-systems.io>
2017-06-23 17:57:26 +0200

    Debug php_lits() 29

--- p7zip.c
+++ p7zip.c
@@ -552,7 +552,7 @@ PHP_FUNCTION(p7zip_list){
         zval entry;
         ZVAL_STR(&entry, filename);
         
-        if(zend_hash_index_add_new(ht, i, &entry) == NULL){
+        if(zend_hash_index_add_new(ht, i, Z_VAL_P(entry)) == NULL){
             zend_string_release(filename);
             RETURN_FALSE;
         }
