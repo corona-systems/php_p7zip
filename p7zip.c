b639389925ec925344b41c38829864abd2927f19
Stefan Schuetz <stefan.schuetz@corona-systems.io>
2017-06-23 13:12:29 +0200

    Corrected errors 18

--- p7zip.c
+++ p7zip.c
@@ -553,7 +553,7 @@ PHP_FUNCTION(p7zip_list){
         zval* entry;
         ZVAL_STR(entry, filename);
         
-        if(zend_hash_index_add_new(ht, i, entry) == FAILURE){
+        if(zend_hash_index_add_new(ht, i, entry) != NULL){
             zend_string_release(filename);
             RETURN_FALSE;
         }
